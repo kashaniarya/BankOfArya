8e33d95ad0efa19aa775cbc362dd394388d6b1aa
Arya Kashani <kashaniarya@users.noreply.github.com>
2020-09-09 12:21:40 -0700

    Update ConnectionFactory.java

--- src/main/java/com/kash/dao/ConnectionFactory.java
+++ src/main/java/com/kash/dao/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	//private static String url = "jdbc:postgresql://localhost/postgres";
 	private static String url = "postgres://qwptitazckhyhv:15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023@ec2-3-223-9-166.compute-1.amazonaws.com:5432/dd70aerbgtn2q6";
 	private static String username = "qwptitazckhyhv";//"postgres";
-	private static String password = "15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023";//"Plk8qas$";
+	private static String password = "15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023";//"p";
 	
 	public static Connection getConnection() {
 		
