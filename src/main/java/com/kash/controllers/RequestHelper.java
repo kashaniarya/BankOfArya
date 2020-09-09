eeb8ff4fa3357e8e9d101728b38bcbe544f7d8d0
Arya Kashani <arya@cryoinnovations.com>
2020-09-09 10:55:25 -0700

    11am

--- src/main/java/com/kash/controllers/RequestHelper.java
+++ src/main/java/com/kash/controllers/RequestHelper.java
@@ -48,100 +48,100 @@ public static void process(HttpServletRequest request, HttpServletResponse respo
 				HomeController.getCreateAccountPage(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/create":
+			case "/api/create":
 				LoginController.create(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/failedCreateAcc":
+			case "/api/failedCreateAcc":
 				HomeController.getFailedCreateAccountPage(request,response);
 				break;
 			
-			case "/FinalProjectV1/api/openAccountPage":
+			case "/api/openAccountPage":
 				HomeController.getOpenAccountPage(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/checkAccountsPage":
+			case "/api/checkAccountsPage":
 				HomeController.getCheckAccountsPage(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/createNewAccountChecking":
+			case "/api/createNewAccountChecking":
 				HomeController.createNewAccountChecking(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/createNewAccountSavings":
+			case "/api/createNewAccountSavings":
 				HomeController.createNewAccountSavings(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/selectAccount":
+			case "/api/selectAccount":
 				HomeController.selectAccount(request,response);
 				break;
 			
-			case "/FinalProjectV1/api/deposit":
+			case "/api/deposit":
 				AccountController.deposit(request, response);
 				break;
 			
-			case "/FinalProjectV1/api/withdraw":
+			case "/api/withdraw":
 				AccountController.withdraw(request, response);
 				break;
 				
-			case "/FinalProjectV1/api/transfer":
+			case "/api/transfer":
 				AccountController.transfer(request, response);
 				break;
 				
-			case "/FinalProjectV1/api/close":
+			case "/api/close":
 				AccountController.close(request, response);
 				break;
 				
-			case "/FinalProjectV1/api/getAdminHomePage":
+			case "/api/getAdminHomePage":
 				AdminController.getAdminHomePage(request,response); 
 				break;
 				
-			case "/FinalProjectV1/api/showAllUsersAndAccounts":
+			case "/api/showAllUsersAndAccounts":
 				AdminController.showAllUsersAndAccounts(request, response);
 				break;
 			
-			case "/FinalProjectV1/api/showAllUsers":
+			case "/api/showAllUsers":
 				//System.out.println("DOES IT EVER HIT HERE");
 				AdminController.showAllUsers(request, response);
 				break;
 			
-			case "/FinalProjectV1/api/showAllAccounts":
+			case "/api/showAllAccounts":
 				AdminController.showAllAccounts(request, response);
 				break;
 				
-			case "/FinalProjectV1/api/adminUpdateUser":
+			case "/api/adminUpdateUser":
 				AdminController.adminUpdateUser(request, response);
 				break;
 				
-			case "/FinalProjectV1/api/adminUpdateAccount":
+			case "/api/adminUpdateAccount":
 				AdminController.adminUpdateAccount(request, response);
 				break;
 			
-			case "/FinalProjectV1/api/showUserById":
+			case "/api/showUserById":
 				AdminController.showUserById(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/showAccountById":
+			case "/api/showAccountById":
 				AdminController.showAccountById(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/showAccountByUsername":
+			case "/api/showAccountByUsername":
 				AdminController.showAccountByUsername(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/showAccountByType":
+			case "/api/showAccountByType":
 				AdminController.showAccountByType(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/showAccountByStatus":
+			case "/api/showAccountByStatus":
 				AdminController.showAccountByStatus(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/editInfoPage":
+			case "/api/editInfoPage":
 				HomeController.getEditInfoPage(request,response);
 				break;
 				
-			case "/FinalProjectV1/api/editInfo":
+			case "/api/editInfo":
 				HomeController.editInfo(request,response);
 				break;
 	
