a1f1cbeb657428c2599753d89c650b3bfc82451c
Arya Kashani <arya@cryoinnovations.com>
2020-09-09 12:29:11 -0700

    confac3

--- src/main/java/com/kash/dao/ConnectionFactory.java
+++ src/main/java/com/kash/dao/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	//private static String url = "jdbc:postgresql://localhost/postgres";
-	private static String url = "jdbc:postgres://qwptitazckhyhv:15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023@ec2-3-223-9-166.compute-1.amazonaws.com:5432/dd70aerbgtn2q6";
+	private static String url = "jdbc:postgres://qwptitazckhyhv:15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023@ec2-3-223-9-166.compute-1.amazonaws.com:5432/dd70aerbgtn2q6/postgres";
 	private static String username = "qwptitazckhyhv";//"postgres";
 	private static String password = "15dcdb59f1d05fc7e02a12f0bc504873ebfebb5b0d7c643988aaf38b7b7e6023";//"p";
 	
